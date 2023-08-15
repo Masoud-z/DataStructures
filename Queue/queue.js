3ffa98e34558e91680bb6597b6a764ba96c8377f
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-15 19:31:14 +0330

    Implement queue with class

--- Queue/queue.js
+++ Queue/queue.js
@@ -8,3 +8,60 @@
 //=>"First come, first serve" order
 //=> Tracking requests for a limited resources
 // => Graph algorithms
+
+//---++Implement Queue with array++---
+// const queue = [];
+// queue.push("a");
+// queue.push("b");
+// queue.push("c");
+// queue.push("d");
+// console.log(queue);
+// queue.shift();
+// console.log(queue);
+
+//---++Create Queue with Class++---
+
+class QueueNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    const newNode = new QueueNode(val);
+    if (this.size === 0) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    this.size++;
+  }
+
+  dequeue() {
+    if (this.size === 0) return null;
+    if (this.size === 1) {
+      this.back = null;
+    }
+    this.front = this.front.next;
+    this.size--;
+  }
+}
+
+const newQueue = new Queue();
+newQueue.enqueue("a");
+newQueue.enqueue("b");
+newQueue.enqueue("c");
+console.log(newQueue.front);
+console.log(newQueue.back);
+newQueue.dequeue();
+console.log(newQueue.front);
