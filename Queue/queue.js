89cea7f273aef9073f37ae5210a3ae19d2c5a376
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-15 19:13:56 +0330

    add definataion of Queue

---
+++ Queue/queue.js
@@ -0,0 +1,10 @@
+//---++What is a Queue?++---
+//=> Collection of items
+//=> Add to back of Queue (enqueue)
+//=> Remove front of queue (dequeue)
+//=> First-In-First-Out (FIFO)
+
+//---++What is a Queue useful for?++---
+//=>"First come, first serve" order
+//=> Tracking requests for a limited resources
+// => Graph algorithms
