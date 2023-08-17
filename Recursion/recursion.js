4c15a63e3e08ddbce2992d5479ac6232fa40fcfc
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-17 19:25:25 +0330

    Sum an array in recursion

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -6,10 +6,16 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(3));
-console.log(factorial(4));
-console.log(factorial(5));
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(5));
 
+function sum(array) {
+  if (array.length === 0) return 0;
+  const rest = array.slice(1);
+  return array[0] + sum(rest);
+}
 
+console.log(sum([1, 2, 3, 10]));
