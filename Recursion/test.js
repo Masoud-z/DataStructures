1a524db8e7da7fc807a724ac6ef127ccc2daef5f
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-23 13:20:10 +0330

    Test recursion sum function

--- Recursion/test.js
+++ Recursion/test.js
@@ -4,8 +4,21 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(3));
-console.log(factorial(4));
-console.log(factorial(5));
+// console.log(factorial(1));
+// console.log(factorial(2));
+// console.log(factorial(3));
+// console.log(factorial(4));
+// console.log(factorial(5));
+
+function sum(array) {
+  if (array.length === 0) return 0;
+  const rest = array.slice(1);
+  return array[0] + sum(rest);
+}
+
+// console.log(sum([]));
+// console.log(sum([1]));
+// console.log(sum([1, 2]));
+// console.log(sum([1, 2, 3]));
+// console.log(sum([1, 2, 3, 4]));
+// console.log(sum([1, 2, 3, 4, 5]));
