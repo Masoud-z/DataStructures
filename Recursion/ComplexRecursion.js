e4dc05c5f2986a6d0a7a658bf878ee9b8313926a
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-23 14:29:53 +0330

    ComplexRecursion combinations  function

--- Recursion/ComplexRecursion.js
+++ Recursion/ComplexRecursion.js
@@ -3,9 +3,26 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
-console.log(fib(5));
-console.log(fib(6));
+// console.log(fib(1));
+// console.log(fib(2));
+// console.log(fib(3));
+// console.log(fib(4));
+// console.log(fib(5));
+// console.log(fib(6));
+
+function combinations(array) {
+  if (array.length === 0) return [[]];
+  const firstEl = array[0];
+  const rest = array.slice(1);
+
+  const combsWithoutFirstEL = combinations(rest);
+  const combsWithFirstEL = [];
+
+  combsWithoutFirstEL.forEach((comb) => {
+    const combWithFirst = [...comb, firstEl];
+    combsWithFirstEL.push(combWithFirst);
+  });
+  return [...combsWithFirstEL, ...combsWithoutFirstEL];
+}
+
+console.log(combinations(["a", "b", "c"]));
