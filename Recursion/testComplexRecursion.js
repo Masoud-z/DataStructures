ab905d6f99b7d1be2d556ae91afc9d8590a99af6
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-29 16:43:58 +0330

    test the combination recursion

--- Recursion/testComplexRecursion.js
+++ Recursion/testComplexRecursion.js
@@ -3,9 +3,41 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
-console.log(fib(5));
-console.log(fib(6));
+// console.log(fib(1));
+// console.log(fib(2));
+// console.log(fib(3));
+// console.log(fib(4));
+// console.log(fib(5));
+// console.log(fib(6));
+
+function combination(elements) {
+  if (elements.length === 0) return [[]];
+  const firstEl = elements[0];
+  const rest = elements.slice(1);
+  const combsWithoutFirstEL = combination(rest);
+  const combsWithFirstEL = [];
+
+  combsWithoutFirstEL.forEach((element) => {
+    const combWithFirstEl = [...element, firstEl];
+    combsWithFirstEL.push(combWithFirstEl);
+  });
+
+  return [...combsWithoutFirstEL, ...combsWithFirstEL];
+}
+
+console.log(combination(["a", "b", "c"]));
+console.log("                                    ");
+console.log("-----------------++++-----------------");
+console.log("                                    ");
+console.log(combination(["a", "b"]));
+console.log("                                    ");
+console.log("-----------------++++-----------------");
+console.log("                                    ");
+console.log(combination(["a"]));
+console.log("                                    ");
+console.log("-----------------++++-----------------");
+console.log("                                    ");
+console.log(combination([]));
+combination(["a", "b", "c"]);
+
+
--- stack/test.js
+++ stack/test.js
@@ -1,45 +1,58 @@
-class StackNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+// class StackNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
 
-class Stack {
-  constructor() {
-    this.top = null;
-    this.size = 0;
-  }
+// class Stack {
+//   constructor() {
+//     this.top = null;
+//     this.size = 0;
+//   }
 
-  push(val) {
-    if (this.size === 0) {
-      this.top = new StackNode(val);
-    } else {
-      const pushedNode = new StackNode(val);
-      pushedNode.next = this.top;
-      this.top = pushedNode;
-    }
-    this.size++;
-  }
+//   push(val) {
+//     if (this.size === 0) {
+//       this.top = new StackNode(val);
+//     } else {
+//       const pushedNode = new StackNode(val);
+//       pushedNode.next = this.top;
+//       this.top = pushedNode;
+//     }
+//     this.size++;
+//   }
 
-  getTop() {
-    return this.top.val;
-  }
+//   getTop() {
+//     return this.top.val;
+//   }
 
-  pop() {
-    if (this.size === 0) return null;
-    const poppedNode = this.top;
-    this.top = this.top.next;
-    this.size--;
-    return poppedNode;
-  }
-}
+//   pop() {
+//     if (this.size === 0) return null;
+//     const poppedNode = this.top;
+//     this.top = this.top.next;
+//     this.size--;
+//     return poppedNode;
+//   }
+// }
 
-const newStack = new Stack();
-newStack.push("a");
-newStack.push("b");
-newStack.push("c");
+// const newStack = new Stack();
+// newStack.push("a");
+// newStack.push("b");
+// newStack.push("c");
 
-console.log(newStack.top);
-newStack.pop();
-console.log(newStack.top);
+// console.log(newStack.top);
+// newStack.pop();
+// console.log(newStack.top);
+
+const result = [
+  "B$u$i$ld",
+  "$t$$h$e",
+  "N$e$x$t",
+  "E$$ra",
+  "$$o$f$",
+  "S$$of$t$wa$r$e",
+  "De$$ve$l$op$me$n$t",
+]
+  .map((s) => s.replace(/\$/g, "").toUpperCase())
+  .join(" ");
+console.log(result);
